8686081445c299487a698eef26ddfd869519ed05
tarun8718 <tarunkumar8718@gmail.com>
2020-07-30 19:52:34 +0530

    Made Changes

---
+++ main.js
@@ -0,0 +1,13 @@
+var map = new GMaps({
+    el: '#map',
+    lat:  40.730610,
+    lng: -73.935242
+});
+
+map.addMarker({
+    lat: 40.700610,
+    lng: -73.997242,
+    title: 'New York'
+});
+
+map.setZoom(8);
